5bac09151bade6b3c922716a0ca5dd187896e4da
bwondessen <bwondessen23@gmail.com>
2024-10-22 17:59:39 -0500

    Display text to notify user wether text is a palindrome or not

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const checkButton = document.getElementById("check-btn");
 const header = document.querySelector("h1");
 const input = document.getElementById("text-input");
+const main = document.querySelector(".main");
+
+let reset = false;
+
+const resultElement = document.createElement("p");
+let resultText = document.createTextNode("");
+
+// let reset = false;
 
 function checkPalindrome() {
-    let filteredStr = input.value.replace(/[^0-9a-z]/gi, '');
-    let reversedStr = filteredStr.split("").reverse().join("");
+    const filteredStr = input.value.replace(/[^0-9a-z]/gi, '');
+    const reversedStr = filteredStr.split("").reverse().join("");
 
     if (filteredStr.toLowerCase() === reversedStr.toLowerCase()) {
+        if (filteredStr) {
+            resultText.nodeValue = "It is a palindrome!";
+
+            resultElement.appendChild(resultText);
+            main.appendChild(resultElement);
+        }
+
         console.log("it is a palindrome!!!");
     } else {
+        if (filteredStr) {
+            resultText.nodeValue = "It is not a palindrome :(";
+
+            resultElement.appendChild(resultText);
+            main.appendChild(resultElement);
+        }
+
         console.log("it is not a palindrome");
     }
+
+    input.value = "";
 };
 
 checkButton.addEventListener("click", checkPalindrome);
