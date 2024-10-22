a33ac4f0ced65853092071af4ccff6aba52554d8
bwondessen <bwondessen23@gmail.com>
2024-10-22 18:46:12 -0500

    Design main section and adjust alignment

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ const header = document.querySelector("h1");
 const input = document.getElementById("text-input");
 const main = document.querySelector(".main");
 
-let reset = false;
-
 const resultElement = document.createElement("p");
+resultElement.id = "result";
 let resultText = document.createTextNode("");
-
-// let reset = false;
 
 function checkPalindrome() {
     const filteredStr = input.value.replace(/[^0-9a-z]/gi, '');
     const reversedStr = filteredStr.split("").reverse().join("");
-
-    if (filteredStr.toLowerCase() === reversedStr.toLowerCase()) {
+    
+    if (!filteredStr) {
+        alert("Please input a value");
+    } else if (filteredStr.toLowerCase() === reversedStr.toLowerCase()) {
         if (filteredStr) {
-            resultText.nodeValue = "It is a palindrome!";
+            resultText.nodeValue = `${input.value} is a palindrome`;
 
             resultElement.appendChild(resultText);
             main.appendChild(resultElement);
@@ -25,7 +24,7 @@ function checkPalindrome() {
         console.log("it is a palindrome!!!");
     } else {
         if (filteredStr) {
-            resultText.nodeValue = "It is not a palindrome :(";
+            resultText.nodeValue = `${input.value} is not a palindrome`;
 
             resultElement.appendChild(resultText);
             main.appendChild(resultElement);
